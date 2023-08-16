0dfc0f2d5455113fc4103b297fd80f0c86b6ceb8
Alihaider0315 <ah712200@gmail.com>
2023-08-16 17:55:51 +0500

    Working Extension With IP Address

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
 let key = "2feb6efb5a9e84164308dcc5732eb53a";
 
 document.addEventListener('DOMContentLoaded', function() {
+  let searchBtn = document.getElementById('search-btn');
   let getWeatherBtn = document.getElementById('get-weather-btn');
+  
+  searchBtn.addEventListener('click', searchWeather);
   getWeatherBtn.addEventListener('click', fetchWeatherData);
 });
 
-function fetchWeatherData() {
+function searchWeather() {
   const searchInput = document.getElementById('search-input');
   const city = searchInput.value.trim();
 
@@ -13,6 +16,22 @@ function fetchWeatherData() {
     return;
   }
 
+  fetchWeather(city);
+}
+
+function fetchWeatherData() {
+  fetch('http://ip-api.com/json')
+    .then(response => response.json())
+    .then(data => {
+      const city = data.city;
+      fetchWeather(city);
+    })
+    .catch(error => {
+      console.error("An error occurred while fetching IP address information:", error);
+    });
+}
+
+function fetchWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
   fetch(url)
